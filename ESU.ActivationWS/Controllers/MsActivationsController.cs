6a0b3f8dc390af673d0ee35b24226e65f67bf133
Yassine Ahmed Bacha <yassine.ahmedbacha@yahoo.fr>
2020-02-17 11:14:43 +0100

    activationws mock ok

---
+++ ESU.ActivationWS/Controllers/MsActivationsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESU.ActivationWS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MsActivationsController : ControllerBase
+    {
+        [HttpGet("{installationId}&{extendedProductId}")]
+        public string Get(string installationId, string extendedProductId)
+        {
+            return $"{installationId}-{extendedProductId}";
+        }
+    }
+}
