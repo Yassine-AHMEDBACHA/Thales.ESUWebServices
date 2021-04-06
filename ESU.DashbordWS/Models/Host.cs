03862eff22e6b483f693b885209604f593f62a5f
Yassine AHMED BACHA <yassine.ahmedbacha@yahoo.fr>
2021-04-07 00:43:18 +0200

    Dashbord OK

--- ESU.DashbordWS/Models/Host.cs
+++ ESU.DashbordWS/Models/Host.cs
@@ -19,6 +19,8 @@ namespace ESU.Data.Models
 
         public string ProductKey { get; set; }
 
+        public string ConfirmationKey { get; set; }
+
         public int? ProductKeyId { get; set; }
 
         public string Status { get; set; }
