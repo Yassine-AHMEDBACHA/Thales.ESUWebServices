81e6c73fae26722852275b6557f6657deba443d4
Yassine AHMED BACHA <yassine.ahmedbacha@yahoo.fr>
2021-04-07 07:49:25 +0200

    authtification

--- ESU.DashbordWS/Controllers/AuthorisationsController.cs
+++ ESU.DashbordWS/Controllers/AuthorisationsController.cs
@@ -1,5 +1,10 @@
 ﻿using ESU.DashbordWS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.DirectoryServices;
+using System.Linq;
 
 namespace ESU.DashbordWS.Controllers
 {
@@ -7,8 +12,12 @@ namespace ESU.DashbordWS.Controllers
     [ApiController]
     public class AuthorisationsController : ControllerBase
     {
-        public AuthorisationsController()
+        private readonly IConfiguration configuration;
+        private readonly ILogger<AuthorisationsController> logger;
+        public AuthorisationsController(IConfiguration configuration, ILogger<AuthorisationsController> logger )
         {
+            this.configuration = configuration;
+            this.logger = logger;
         }
 
         [HttpPost()]
@@ -19,9 +28,42 @@ namespace ESU.DashbordWS.Controllers
                 user.IsAuthenticated = true;
                 user.Role = "Admin";
             }
-            else user.IsAuthenticated = false;
+            else
+            {
+                user.IsAuthenticated = this.CheckUser(user);
+            }
 
             return user;
+        }
+
+        private bool CheckUser(User user)
+        {
+            string path = this.configuration.GetValue<string>("LDAP_ROOT");
+            string domain = this.configuration.GetValue<string>("LDAP_DOMAIN");
+            var domainAndUsername = domain + @"\" + user.UserName;
+            try
+            {
+                var entry = new DirectoryEntry(path, domainAndUsername, user.Password);
+                logger.LogInformation($"User {user.UserName} try to log in!");
+                var obj = entry.NativeObject;
+                var directorySearcher = new DirectorySearcher(entry);
+                directorySearcher.Filter = $"(sAMAccountName={user.UserName})";
+                directorySearcher.PropertiesToLoad.Add("givenName");
+                directorySearcher.PropertiesToLoad.Add("sn");
+                directorySearcher.PropertiesToLoad.Add("memberOf");
+                var result = directorySearcher.FindOne();
+                var properties = result.Properties;
+                if(properties == null)
+                {
+                    throw new Exception("No more informations");
+                }
+            }
+            catch(Exception ex  )
+            {
+                this.logger.LogError(ex.Message);
+                return false;
+            }
+            return true;
         }
     }
 }
